d82df7f5de8a6ccda495e4c4482799c0cb915e18
Joni De Borger <joni.de.borger@ehb.be>
2022-10-05 16:07:24 +0200

    finished exercise 1

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,42 @@
 "use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawName();
+
+function drawName() {
+
+    //J
+    context.strokeStyle = "red";
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(200, 50);
+    context.lineTo(200, 250);
+    context.lineTo(150, 275);
+    context.lineTo(100, 250);
+    context.stroke();
+
+    //O
+    context.strokeStyle = "green";
+    context.beginPath();
+    context.moveTo(300, 50);
+    context.lineTo(350, 150);
+    context.lineTo(300, 250);
+    context.lineTo(250, 150);
+    context.lineTo(300, 50);
+    context.stroke();
+
+
+    //N
+    context.strokeStyle = "blue";
+    context.beginPath();
+    context.moveTo(400, 50);
+    context.lineTo(400, 250);
+    context.moveTo(400, 50);
+    context.lineTo(500, 250);
+    context.stroke();
+
+}
